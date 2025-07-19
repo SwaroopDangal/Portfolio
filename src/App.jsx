ddc272668b9ac376f67049a52392ad20d0a43e22
SwaroopDangal <swaroopdangal732@gmail.com>
2025-07-19 11:36:13 +0545

    fixed error

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import NavbarMain from "./components/Navbar/NavbarMain";
 import HeroMain from "./components/HeroSection/HeroMain";
 import HeroGradient from "./components/HeroSection/HeroGradient";
-import SubHeroMain from "./components/HeroSection/subHeroSection";
+import SubHeroMain from "./components/HeroSection/SubHeroSection";
 import AboutMeMain from "./components/AboutMeSection/AboutMeMain";
 import HelperSection from "./components/HelperSection";
 import SkillMain from "./components/SkillSection/SkillMain";
